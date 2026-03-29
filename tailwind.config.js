bc44b1585ba7c3a183ef19a3acfd2205218d3e1b
Md. Armaan Hossain Chaklader <58468047+armaanchakla@users.noreply.github.com>
2026-03-29 18:48:15 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Fira Code", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
